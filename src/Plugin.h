f202a9b4c61d22d7c6e7aab4c6f531a599c53f4d
Anthony Coddington <anthony.coddington@endace.com>
2018-02-16 19:41:43 +1300

    Initial commit of bro init-plugin generated skeleton

---
+++ src/Plugin.h
@@ -0,0 +1,22 @@
+
+#ifndef BRO_PLUGIN_ENDACE_DAG
+#define BRO_PLUGIN_ENDACE_DAG
+
+#include <plugin/Plugin.h>
+
+namespace plugin {
+namespace Endace_DAG {
+
+class Plugin : public ::plugin::Plugin
+{
+protected:
+	// Overridden from plugin::Plugin.
+	plugin::Configuration Configure() override;
+};
+
+extern Plugin plugin;
+
+}
+}
+
+#endif
---
+++ tests/dag/show-plugin.bro
@@ -0,0 +1,2 @@
+# @TEST-EXEC: bro -NN Endace::DAG >output
+# @TEST-EXEC: btest-diff output
