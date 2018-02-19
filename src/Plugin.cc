9d31f3a873c4b6ee16b441c3f6245fe98ff58f2a
Anthony Coddington <anthony.coddington@endace.com>
2018-02-19 18:35:55 +1300

    Add bro-pkg.meta
    Also add description, wording roughly matches other iosource plugins.

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -12,7 +12,7 @@ plugin::Configuration Plugin::Configure()
 
 	plugin::Configuration config;
 	config.name = "Endace::DAG";
-	config.description = "<Insert description>";
+	config.description = "Packet acquisition via Endace DAG capture cards";
 	config.version.major = 0;
 	config.version.minor = 1;
 	return config;
