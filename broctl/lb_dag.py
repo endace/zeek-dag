69c4b6451d0071968ed713c119b9d4a600abbb8e
Anthony Coddington <anthony.coddington@endace.com>
2018-02-23 15:54:15 +1300

    Add basic BroCtl plugin
    Just supports endace::(dag)X:Y
    Reads PFRINGFirstAppInstance for first stream number. Not defining any plugin specific options yet.

---
+++ broctl/lb_dag.py
@@ -0,0 +1,40 @@
+
+# This plugin sets necessary environment variables to run Bro with
+# PF_RING load balancing.
+
+import BroControl.plugin
+import BroControl.config
+
+class LBDAG(BroControl.plugin.Plugin):
+    def __init__(self):
+        super(LBDAG, self).__init__(apiversion=1)
+
+    def name(self):
+        return "lb_dag"
+
+    def pluginVersion(self):
+        return 1
+
+    def init(self):
+        useplugin = False
+        first_app_instance = int(BroControl.config.Config.pfringfirstappinstance)
+        app_instance = first_app_instance
+        host = None
+        for nn in self.nodes():
+            if nn.type != "worker" or nn.lb_method != "dag":
+                continue
+
+            # Reset stream numbers for different hosts
+            if nn.host != host:
+                app_instance = first_app_instance
+                host = nn.host
+
+            useplugin = True
+
+            if nn.interface.startswith("endace::"):
+                # For the case where a user is running dag HLB
+                nn.interface = "%s:%d" % (nn.interface, app_instance*2)
+
+            app_instance += 1
+
+        return useplugin
