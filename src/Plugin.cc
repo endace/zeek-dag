d1f767f85f0ff2d5fe5154d83ce2fc36813a51a4
Anthony Coddington <anthony.coddington@endace.com>
2020-09-18 15:32:49 +1200

    Bump version number to 0.4

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -16,6 +16,6 @@ plugin::Configuration Plugin::Configure()
 	config.name = "Endace::DAG";
 	config.description = "Packet acquisition via Endace DAG capture cards";
 	config.version.major = 0;
-	config.version.minor = 3;
+	config.version.minor = 4;
 	return config;
 	}
