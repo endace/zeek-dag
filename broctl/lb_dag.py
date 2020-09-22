858b2e35cef7aaa415c25c3f150ca7b5211e961f
Anthony Coddington <anthony.coddington@endace.com>
2020-09-22 16:06:09 +1200

    Add card load balancing support to zeekctl plugin
    Add dag_lb_method node.cfg option, supporting two modes: card and stream. Default is stream to preserve existing behaviour, new card option is for multiple vDAGs each with one stream as currently used in Application Dock.
    Extract starting card and stream number from interface string.
    Reset numbering when host, interface string or dag_lb_method changes (including if not specified).
    Fall back to PFRingFirstAppInstance like before if the user doesn't specify a start string (defaults to 0), this is now deprecated.
    Rename plugin to just "dag" instead of "lb_dag" so we get a more sensible option prefix.

--- broctl/lb_dag.py
+++ broctl/lb_dag.py
@@ -11,12 +11,14 @@ except ImportError:
     from BroControl import plugin
     from BroControl import config
 
+import re
+
 class LBDAG(plugin.Plugin):
     def __init__(self):
         super(LBDAG, self).__init__(apiversion=1)
 
     def name(self):
-        return "lb_dag"
+        return "dag"
 
     def pluginVersion(self):
         return 1
@@ -24,22 +26,56 @@ class LBDAG(plugin.Plugin):
     def init(self):
         useplugin = False
         first_app_instance = int(config.Config.pfringfirstappinstance)
-        app_instance = first_app_instance
         host = None
+        last_interface = None
+        last_dag_lb_method = None
+        dag_lb_method = None
         for nn in self.nodes():
             if nn.type != "worker" or not nn.interface.startswith("endace::") or not nn.lb_procs or nn.lb_method != "custom":
                 continue
 
-            # Reset stream numbers for different hosts
-            if nn.host != host:
-                app_instance = first_app_instance
-                host = nn.host
-
             useplugin = True
 
-            # For the case where a user is running dag HLB
-            nn.interface = "%s:%d" % (nn.interface, app_instance*2)
+            dag_lb_method = nn.dag_lb_method if nn.dag_lb_method else "stream"
 
-            app_instance += 1
+            # Reset for different hosts or if the user-specified card or stream number changes
+            if nn.host != host or nn.interface != last_interface or dag_lb_method != last_dag_lb_method:
+                host = nn.host
+                last_interface = nn.interface
+                last_dag_lb_method = dag_lb_method
+
+                match = re.match("endace::(.*?)([0-9]*)(?:\:([0-9]*))?$", nn.interface)
+                dag_card_name, dag_card_num, dag_stream_num = match.groups()
+                card_name = dag_card_name if dag_card_name is not None else "dag"
+
+                if dag_card_num is not None:
+                    card_num = int(dag_card_num)
+                elif dag_lb_method == "card":
+                    card_num = first_app_instance
+                else:
+                    card_num = 0
+
+                if dag_stream_num is not None:
+                    stream_num = int(dag_stream_num)
+                elif dag_lb_method == "stream":
+                    stream_num = first_app_instance
+                else:
+                    stream_num = None
+
+            if stream_num is not None:
+                nn.interface = "endace::%s%d:%d" % (card_name, card_num, stream_num)
+            else:
+                nn.interface = "endace::%s%d" % (card_name, card_num)
+
+            if dag_lb_method == "stream":
+                # For the case where a user is running dag HLB
+                stream_num += 2
+            elif dag_lb_method == "card":
+                # Multiple dag cards each with one stream
+                card_num += 1
 
         return useplugin
+
+    # TODO: do we want to add these as options as well?
+    def nodeKeys(self):
+        return ["lb_method"]
